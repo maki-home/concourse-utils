ed38779120ce81a4f70112f47c49a4ee5e009b90
making <makingx@gmail.com>
2017-03-27 00:20:47 +0900

    Add cd

--- tasks/bump-to-next-develop-version.sh
+++ tasks/bump-to-next-develop-version.sh
@@ -13,6 +13,9 @@ cd release-out
 git remote add -f prod $BASEDIR/repo-prod
 git merge --no-edit prod/master
 echo "Bump to $VERSION"
+if [ "x${PROJECT}" != "x" ];then
+    cd ${PROJECT}
+fi
 mvn versions:set -DnewVersion=${VERSION} -DallowSnapshots -Dmaven.repo.local=$M2REPO
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
