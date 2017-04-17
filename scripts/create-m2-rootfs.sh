c4d053a8901031d874e145cab2169c598cfd4d96
Toshiaki Maki <tmaki@pivotal.io>
2017-04-18 03:23:40 +0900

    Update create-m2-rootfs.sh

--- scripts/create-m2-rootfs.sh
+++ scripts/create-m2-rootfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd repo
-mvn clean install -Dmaven.repo.local=../m2/rootfs/opt/m2
+mvn clean install -Dmaven.repo.local=../m2/rootfs/opt/m2 -DskipTests=true
 cd ../m2
 tar -C rootfs -cf rootfs.tar .
 mv rootfs.tar ../output
