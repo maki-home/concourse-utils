9c548b661cc8badbb4c303c267ee4ddec7052300
making <makingx@gmail.com>
2017-04-22 02:38:53 +0900

    Add port check

---
+++ scripts/check.py
@@ -0,0 +1,30 @@
+import sys
+import socket
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Check tcp connectivity')
+parser.add_argument('--host', dest='host', type=str, help='target host')
+parser.add_argument('--port', dest='port', type=int, help='target port')
+parser.add_argument('--interval', dest='interval', type=int, default='3', help='interval to check')
+parser.add_argument('--retries', dest='retries', type=int, default='3', help='number of retries')
+args = parser.parse_args()
+
+def check_tcp_connectivity(host, port, interval=0, retries=1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    for x in range(retries):
+        try:
+            s.connect((host, port))
+            s.close()
+            return True
+        except socket.error as e:
+            time.sleep(interval)
+
+    s.close()
+    return False
+
+if check_tcp_connectivity(args.host, args.port, args.interval, args.retries):
+    sys.exit(0)
+else:
+    sys.exit(1)
---
+++ scripts/wait-until-port-available.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+host=$1
+port=$2
+
+echo "wait $host:$port"
+
+python check.py --host $host --port $port
+ret=$?
+
+for i in `seq 0 10`;do
+    if [ $ret == 0 ];then
+        echo "ok"
+        exit 0;
+    fi
+    echo "wait"
+    sleep 1
+    python check.py --host $host --port $port
+    ret=$?
+done
+
+echo "ng"
+exit 1
