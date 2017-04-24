81f69858b8680e44a637dfe8cd2031e2a4ad28ca
making <makingx@gmail.com>
2017-04-24 21:33:22 +0900

    Add BASEDIR

--- scripts/bosh-create-release.sh
+++ scripts/bosh-create-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+BASEDIR=`pwd`
 if [ -d repo-version ];then
     VERSION=`cat repo-version/number`
     BOSH_OPTS="--version ${VERSION} --final"
@@ -9,7 +9,7 @@ else
 fi
 
 cd repo
-utils/scripts/generate-private-yml.sh
+${BASEDIR}/utils/scripts/generate-private-yml.sh
 mkdir src
 bosh -n create release --force --with-tarball ${BOSH_OPTS}
 mv ./*releases/*/*.tgz ../releases/
