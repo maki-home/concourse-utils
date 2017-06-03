500d23656d0deeb1a9dedea6860e0fdcd5b02c14
Toshiaki Maki <tmaki@pivotal.io>
2017-06-03 15:27:40 +0900

    Update bump-to-next-develop-version.sh

--- tasks/bump-to-next-develop-version.sh
+++ tasks/bump-to-next-develop-version.sh
@@ -5,7 +5,7 @@ BASEDIR=`pwd`
 M2REPO=$BASEDIR/m2/rootfs/opt/m2
 echo "M2REPO=$M2REPO"
 VERSION=`cat repo-version/number`-SNAPSHOT
-MESSAGE="[ci skip] Bump to Next Development Version ($VERSION)"
+MESSAGE="Bump to Next Development Version ($VERSION)"
 
 shopt -s dotglob
 mv -f repo-staging/* release-out/
